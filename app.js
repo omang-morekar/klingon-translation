da7fddfe07700a4aed135a1e52eca61033c09c49
Omang Morekar <[omangmorekar68@gmail.com]>
2020-12-06 15:27:04 +0530

    updated style

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+const buttonTranslate = document.querySelector("#btn-translate")
+const inputText = document.querySelector("#ip")
+const outputText = document.querySelector("#op")
+
 const url = 'https://api.funtranslations.com/translate/klingon.json'
 
-console.log('Omang');
+function errorHandler() {
+  console.log('An error has occured.');
+
+  alert('It seems an error has occured, please try again after sometime.')
+}
+
+
+
+function translate() {
+
+  var ipTx = inputText.value
+
+  function translateUrl(ipTx) {
+    return url + "?" + "text=" + ipTx
+  }
+
+  fetch(translateUrl(ipTx))
+  .then(serRepsonse => serRepsonse.json())
+  .then(json => {
+    outputText.innerText = json.contents.translated
+  })
+
+  .catch(errorHandler)
+}
+
+buttonTranslate.addEventListener('click', translate)
